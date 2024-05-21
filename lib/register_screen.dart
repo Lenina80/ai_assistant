ad1b152f7ce93ed14c09892232578118edaad1a6
Liubov Kudriavtseva <kudrluba2003@gmail.com>
2024-05-21 18:35:38 +0300

    llm-43: fix button paddings

--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -57,68 +57,77 @@ class _RegisterScreenState extends State<RegisterScreen> {
                       },
                     ),
                     SizedBox(height: 12),
-                    ElevatedButton(
-                      onPressed: () async {
-                        FocusScope.of(context).unfocus();
-                        _formKey.currentState!.save();
-                        final credential = await signUpWithEmailAndPassword("$_email", "$_password");
-                        if (credential != null) {
-                          // User signed up successfully
-                          Navigator.of(context).pushReplacement(
-                            MaterialPageRoute(builder: (context) => ChatPage()),
-                          );
-                        } else {
-                          // Handle sign up error
-                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                            content: Text('Error auth with email'),
-                          )
-                          );
-                        }
-                      },
-                      child: Text('Sing Up'),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16.0),
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          FocusScope.of(context).unfocus();
+                          _formKey.currentState!.save();
+                          final credential = await signUpWithEmailAndPassword("$_email", "$_password");
+                          if (credential != null) {
+                            // User signed up successfully
+                            Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(builder: (context) => ChatPage()),
+                            );
+                          } else {
+                            // Handle sign up error
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text('Error auth with email'),
+                            )
+                            );
+                          }
+                        },
+                        child: Text('Sing Up'),
+                      ),
                     ),
-                    ElevatedButton(
-                      onPressed: () async {
-                        FocusScope.of(context).unfocus();
-                        _formKey.currentState!.save();
-                        final credential = await signInWithEmailAndPassword("$_email", "$_password");
-                        if (credential != null) {
-                          // User signed in successfully
-                          Navigator.of(context).pushReplacement(
-                            MaterialPageRoute(builder: (context) => ChatPage()),
-                          );
-                        } else {
-                          // Handle sign in error
-                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                            content: Text('Error login with email'),
-                          )
-                          );
-                        }
-                      },
-                      child: Text('Sign In'),
+                    Padding(
+                        padding: const EdgeInsets.only(top: 16.0),
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            FocusScope.of(context).unfocus();
+                            _formKey.currentState!.save();
+                            final credential = await signInWithEmailAndPassword("$_email", "$_password");
+                            if (credential != null) {
+                              // User signed in successfully
+                              Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(builder: (context) => ChatPage()),
+                              );
+                            } else {
+                              // Handle sign in error
+                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                content: Text('Error login with email'),
+                              )
+                              );
+                            }
+                          },
+                          child: Text('Sign In'),
+                        ),
                     ),
-                    ElevatedButton(
-                      onPressed: () async {
-                        final credential = await signInWithGoogle();
-                        if (credential != null) {
-                          print('Success login with Goggle');// User signed in with Google
-                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                            content: Text('Success login with Goggle'),
-                          )
-                          );
-                          // Переход на экран чата после успешной регистрации
-                          Navigator.of(context).pushReplacement(
-                            MaterialPageRoute(builder: (context) => ChatPage()),
-                          );
-                        } else {
-                          print('Error login with Goggle');// Handle sign in error or inform user Google sign-in not supported
-                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                            content: Text('Error login with Goggle'),
-                          )
-                          );
-                        }
-                      },
-                      child: Text("Sign In with Google"),
+                    Padding(
+                        padding: const EdgeInsets.only(top: 16.0),
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            final credential = await signInWithGoogle();
+                            if (credential != null) {
+                              print('Success login with Goggle');// User signed in with Google
+                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                content: Text('Success login with Goggle'),
+                              )
+                              );
+                              // Переход на экран чата после успешной регистрации
+                              Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(builder: (context) => ChatPage()),
+                              );
+                            } else {
+                              print('Error login with Goggle');// Handle sign in error or inform user Google sign-in not supported
+                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                content: Text('Error login with Goggle'),
+                              )
+                              );
+                            }
+                          },
+                          child: Text("Sign In with Google"),
+                        ),
                     ),
                   ],
                 ),
